a2de874f859e482b3c9646523b51e55e44393114
RATSENO <onestar1014@gmail.com>
2018-07-30 00:29:31 +0900

    [MOD]회원 가입 중복체크 로직 매퍼 > 아이디 중복체크, 이메일 중복체크, 비밀번호 중복체크

--- src/main/java/com/ratseno/board/member/mapper/MemberMapper.java
+++ src/main/java/com/ratseno/board/member/mapper/MemberMapper.java
@@ -1,10 +1,19 @@
 package com.ratseno.board.member.mapper;
 
 import com.ratseno.board.common.annotation.Mapper;
+import com.ratseno.board.member.model.req.MemberJoinReq;
 
 @Mapper
 public interface MemberMapper {
 	
 	public String getTime();
-
+	
+	/*ID 중복 체크*/
+	public String selectDuplicationCheckUserId(MemberJoinReq memberJoinReq);
+	
+	/*이메일 중복 체크*/
+	public String selectDuplicationCheckUserEmail(MemberJoinReq memberJoinReq);
+	
+	/*비밀번호 중복 체크*/
+	public String selectDuplicationCheckUserPwd(MemberJoinReq memberJoinReq);
 }
--- src/main/java/com/ratseno/board/member/model/req/MemberJoinReq.java
+++ src/main/java/com/ratseno/board/member/model/req/MemberJoinReq.java
@@ -1,5 +1,21 @@
 package com.ratseno.board.member.model.req;
 
-public class MemberJoinReq {
+import com.ratseno.board.common.model.CommonVO;
+
+import lombok.Data;
+
+@Data
+public class MemberJoinReq extends CommonVO{
+	
+	//회원 번호
+	private Integer userNo;
+	//회원 ID
+	private String userId;
+	//회원 비밀번호
+	private String userPwd;
+	//회원 이메일
+	private String userEmail;
+	//회원 전화번호
+	private String userPhoneNumber;
 
 }
