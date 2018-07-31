0cd8463e76c23feb07cc10ea6cdd28f40e32ee7d
RATSENO <onestar1014@gmail.com>
2018-07-31 22:16:54 +0900

    [ADD]인터셉터 샘플 추가

---
+++ src/main/java/com/ratseno/board/common/interceptor/SampleInterceptor.java
@@ -0,0 +1,7 @@
+package com.ratseno.board.common.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SampleInterceptor extends HandlerInterceptorAdapter{
+
+}
